2657227f7c40627349d6677b5c29fdf9430e1488
gpelelis <gpelelis@gmail.com>
2018-10-23 09:38:28 +0300

    add example I

--- html/1-walker.js
+++ html/1-walker.js
@@ -1,7 +1,36 @@
+class Walker {
+	constructor() {
+		this.x = width/2;
+		this.y = height/2;
+	}
+
+	display() {
+		stroke(0);
+		point(this.x, this.y);
+	}
+
+	step() {
+		const choice = Math.trunc(random(4));
+
+		if (choice == 0)
+			this.x++;
+		else if (choice == 1)
+			this.x--;
+		else if (choice == 2)
+			this.y++;
+		else if (choice == 3)
+			this.y--;
+	}
+}
+
+let w;
 function setup() {
-  // put setup code here
+	createCanvas(640, 360);
+	w = new Walker();
+	background(255);
 }
 
 function draw() {
-  // put drawing code here
+  w.step();
+  w.display();
 }
